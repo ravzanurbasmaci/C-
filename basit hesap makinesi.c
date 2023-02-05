e877829c114789ae16f92521325eda248b131fc2
ravzanurbasmaci <110766948+ravzanurbasmaci@users.noreply.github.com>
2023-02-06 00:15:29 +0300

    Add files via upload

---
+++ basit hesap makinesi.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+float hesap(float a,float b,char islem){
+	
+	switch(islem){
+	 
+	case '+' : return a+b; break;
+	case '-' : return a-b; break;
+	case '*': return a*b; break;
+	case '/': return a/b; break;
+	default: printf("tanimsiz islem secimi\n");
+	
+	}
+	
+	
+}
+
+int main(){
+	float a,b,csonuc;
+	char islem; 
+
+	while(1){
+		printf("\n1-Hesap Makinesi\n2-Cikis\nSeciminiz:\n");
+		scanf("%d",&c);
+		if(c==1){	
+			printf("ilk sayiyi giriniz: \n");
+			scanf("%d",&a);
+			printf("ikinci sayiyi giriniz: \n");
+			scanf("%d",&b);
+			printf("islem tipini giriniz: \n");
+			scanf("%s",&islem);
+			sonuc = hesap(a,b,islem);	
+			printf("sonuc= %f ",sonuc);
+			}
+		else break;
+	}
+		
+	return 0;
+}
